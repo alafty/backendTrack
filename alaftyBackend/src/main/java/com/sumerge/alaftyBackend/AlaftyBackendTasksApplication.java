62ff4e22bfc78c3edc7b91539a5c28192b28e094
alafty <mohamedelalafty@yahoo.com>
2024-07-30 17:02:01 +0300

    project init

---
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/AlaftyBackendTasksApplication.java
@@ -0,0 +1,13 @@
+package com.sumerge.alaftyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaftyBackendTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaftyBackendTasksApplication.class, args);
+	}
+
+}
