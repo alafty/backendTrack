9bc6ec87216238e59de4b1dc7000c8ae3c29a5ef
alafty <mohamedelalafty@yahoo.com>
2024-07-30 19:52:36 +0300

    created models

---
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/Models/Booking.java
@@ -0,0 +1,10 @@
+package com.sumerge.alaftyBackend.Models;
+
+
+public class Booking {
+    public String name;
+
+    public Booking(String name) {
+        name = name;
+    }
+}
---
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/Models/Course.java
@@ -0,0 +1,10 @@
+package com.sumerge.alaftyBackend.Models;
+
+
+public class Course {
+    public String name;
+
+    public Course(String name) {
+        name = name;
+    }
+}
