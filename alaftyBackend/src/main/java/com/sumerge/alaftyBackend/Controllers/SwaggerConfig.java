82c23085da89f06747708f893c626a03580f558c
alafty <mohamedelalafty@yahoo.com>
2024-08-01 16:55:54 +0300

    added Swagger for the docs

---
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/Controllers/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.sumerge.alaftyBackend.Controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket atividadeApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sumerge.alaftyBackend"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Atividades API REST",
+                "API REST de cadastro de atividades.",
+                "1.0",
+                "Terms of Service",
+                new Contact("João VR", "www.una.br/",
+                        " "),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
