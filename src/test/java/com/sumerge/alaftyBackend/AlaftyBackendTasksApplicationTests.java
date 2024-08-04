4ba3f8ff2ff9c2b9d8d483189577b802d913b5ae
alafty <mohamedelalafty@yahoo.com>
2024-08-04 18:10:55 +0300

    removed Spring Boot annotations from test

--- src/test/java/com/sumerge/alaftyBackend/AlaftyBackendTasksApplicationTests.java
+++ src/test/java/com/sumerge/alaftyBackend/AlaftyBackendTasksApplicationTests.java
@@ -1,12 +1,9 @@
 package com.sumerge.alaftyBackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AlaftyBackendTasksApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
