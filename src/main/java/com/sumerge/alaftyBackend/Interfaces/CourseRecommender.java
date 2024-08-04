e8f33d77931ff5fe3afa3687924160a408f8ee14
alafty <mohamedelalafty@yahoo.com>
2024-08-04 18:03:38 +0300

    minor changes

---
+++ src/main/java/com/sumerge/alaftyBackend/Interfaces/CourseRecommender.java
@@ -0,0 +1,12 @@
+package com.sumerge.alaftyBackend.Interfaces;
+
+
+import com.sumerge.alaftyBackend.Models.Course;
+
+import java.util.List;
+
+public interface CourseRecommender {
+
+    List<Course> recommendedCourses();
+
+}
