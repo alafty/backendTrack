ca0d8262014a7536cd93bb502a64dffebabbc2bd
alafty <mohamedelalafty@yahoo.com>
2024-08-06 14:27:32 +0300

    metadata

--- alaftyBackend/src/main/java/com/sumerge/alaftyBackend/AlaftyBackendTasksApplication.java
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/AlaftyBackendTasksApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Component;
 
 import java.awt.print.Book;
