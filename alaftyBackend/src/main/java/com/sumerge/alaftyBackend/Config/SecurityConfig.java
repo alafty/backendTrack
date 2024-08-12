cd873fcbee3011e7f042f4bc3e6f22d71d26fb4e
alafty <mohamedelalafty@yahoo.com>
2024-08-12 18:02:13 +0300

    created Security Config file

---
+++ alaftyBackend/src/main/java/com/sumerge/alaftyBackend/Config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.sumerge.alaftyBackend.Config;
+
+import com.sumerge.alaftyBackend.Controllers.UserDetailsServiceImpl;
+import com.sumerge.alaftyBackend.Filters.xValidationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsServiceImpl = userDetailsService;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/courses/discover/").permitAll()
+                        .requestMatchers("/courses/view/**", "/courses/add/**", "/courses/update/**").authenticated()
+                )
+                .addFilterAfter(new xValidationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(authenticationProvider())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    @Bean public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsServiceImpl);
+        authProvider.setPasswordEncoder(userDetailsServiceImpl.passwordEncoder);
+        return authProvider;
+    }
+
+
+}
